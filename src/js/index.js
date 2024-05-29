694d1ceecc2ff8919ea35347526a8c4d58dc6e66
liliang <1497809786@qq.com>
2024-05-29 16:26:53 +0800

    test: dayjs

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import { dva } from './dev.js'
 import { base, kkk } from './test/base.js'
+import dayjs from 'dayjs'
 dva()
+console.log('time', dayjs().format('YYYY-MM-DD HH:mm:ss'));
 console.log('base', base);
 console.log('kkk', kkk);
